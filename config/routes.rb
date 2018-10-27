82417c3987ab25ad9da5c604bafcbee7b4087901
neos2019 <nestor1984@gmail.com>
2018-10-27 09:54:16 -0300

    initial

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+    get 'pages/team_mates'
+    get 'pages/index'
+  
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
+   
+
